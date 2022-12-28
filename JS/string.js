faf2251a2902fcd2736e823de026c9193eb56425
[engineermansha] <[manshauaf@gmail.com]>
2022-12-28 16:51:27 +0500

    string method recall

--- JS/string.js
+++ JS/string.js
@@ -1,5 +1,18 @@
 const str = "Hello string Method";
+const str2 = 'AppsGenii';
 console.log(str.charAt(7));
 //output is t
 console.log(str.slice(0, 6));
 console.log(str.split())
+console.log(str.search('M'))
+//output 13
+console.log(str.search("Method"));
+//output 13
+console.log(str.indexOf('g'))
+//check the index from to last 
+console.log(str.lastIndexOf('g'))
+console.log(str.concat(str2));
+console.log(str.blink())
+console.log(str.bold());
+//its provide unicode of the charcters
+console.log(str.charCodeAt(13))
